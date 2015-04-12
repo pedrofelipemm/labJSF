0e4e6bc74fd8f20403f375473a0c0cc4dee1ab8d
Pedro Felipe Marques Moreira <pedrofelipemm@gmail.com>
2015-04-11 22:49:29 -0300

    FileUpload

--- src/main/java/br/com/jsflab/view/mb/AbstractMB.java
+++ src/main/java/br/com/jsflab/view/mb/AbstractMB.java
@@ -10,33 +10,47 @@ import javax.faces.component.UIViewRoot;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.context.Flash;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 @ManagedBean
 @SuppressWarnings("serial")
 public abstract class AbstractMB implements Serializable {
 
+    protected static final int BUFFER_SIZE = 1024;
+    protected static final String BUNDLE_NAME = "bundle";
+    protected static final String EMPTY_STRING = "";
+
     @PostConstruct
-    public abstract void inicializar();
+    protected abstract void inicializar();
 
     public FacesContext getCurrentInstance() {
-
         return FacesContext.getCurrentInstance();
     }
 
-    public UIViewRoot getViewRoot() {
+    public ServletContext getContext() {
+        return (ServletContext) getExternalContext().getContext();
+    }
 
+    public String getRealPath(String path) {
+        return getContext().getRealPath(path);
+    }
+
+    public String getRealPath() {
+        return getRealPath(EMPTY_STRING);
+    }
+
+    public UIViewRoot getViewRoot() {
         return getCurrentInstance().getViewRoot();
     }
 
     public ExternalContext getExternalContext() {
-
         return getCurrentInstance().getExternalContext();
     }
 
     private Flash getFlash() {
-
         return getExternalContext().getFlash();
     }
 
@@ -51,32 +65,26 @@ public abstract class AbstractMB implements Serializable {
     }
 
     public Object getFlashObject(String key) {
-
         return getFlash().get(key);
     }
 
     public HttpServletRequest getRequest() {
-
         return (HttpServletRequest) getExternalContext().getRequest();
     }
 
     public HttpServletResponse getResponse() {
-
         return (HttpServletResponse) getExternalContext().getResponse();
     }
 
     public ResourceBundle getResourceBundle() {
-
-        return ResourceBundle.getBundle("bundle", getViewRoot().getLocale());
+        return ResourceBundle.getBundle(BUNDLE_NAME, getViewRoot().getLocale());
     }
 
     public String getMessage(String key) {
-
         if (key == null) {
             //TODO i18n
             throw new IllegalArgumentException("Key não pode ser nula");
         }
-
         return getResourceBundle().getString(key);
     }
 
@@ -105,5 +113,16 @@ public abstract class AbstractMB implements Serializable {
             String parametroInternacionalizado = getMessage(parametro);
             parametro = (parametroInternacionalizado == null) ? parametro : parametroInternacionalizado;
         }
+    }
+
+    protected String getFileNameFromPart(Part part) {
+        final String partHeader = part.getHeader("content-disposition");
+        for (String content : partHeader.split(";")) {
+            if (content.trim().startsWith("filename")) {
+                String fileName = content.substring(content.indexOf('=') + 1).trim().replace("\"", "");
+                return fileName;
+            }
+        }
+        return null;
     }
 }
--- src/main/java/br/com/jsflab/view/mb/ImageMB.java
+++ src/main/java/br/com/jsflab/view/mb/ImageMB.java
@@ -1,56 +1,74 @@
 package br.com.jsflab.view.mb;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.servlet.http.Part;
 
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.UploadedFile;
 
 import br.com.jsflab.service.ImageService;
 
-@ManagedBean(name="imageMB")
+@ManagedBean(name = "imageMB")
 @ViewScoped
-public class ImageMB extends AbstractMB{
+public class ImageMB extends AbstractMB {
 
-	private static final long serialVersionUID = -3215385844256696577L;
+    private static final long serialVersionUID = -3215385844256696577L;
 
-	@ManagedProperty(value="#{imageService}")
-	private ImageService imageService;
+    @ManagedProperty(value = "#{imageService}")
+    private ImageService imageService;
 
-	private File imagem;
+    private Part imagem;
 
-	@Override
-	public void inicializar() {
+    @Override
+    public void inicializar() {
 
-	}
+    }
 
-	public void handleFileUpload(FileUploadEvent fileUploadEvent) {
+    public void uploadImage() throws IOException {
+        if (imagem.getSize() > 0) {
+            String fileName = getFileNameFromPart(imagem);
+            File outputFile = new File(getRealPath() + File.separator + "WEB-INF" + File.separator + fileName);
+            InputStream inputStream = imagem.getInputStream();
+            OutputStream outputStream = new FileOutputStream(outputFile);
+            byte[] buffer = new byte[BUFFER_SIZE];
+            int bytesRead = 0;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+            outputStream.close();
+            inputStream.close();
+        }
+    }
 
-		UploadedFile file = fileUploadEvent.getFile();
-		System.out.println(file.getClass().getName());
-	}
+    public void handleFileUpload(FileUploadEvent fileUploadEvent) {
 
-	public ImageService getImageService() {
+        UploadedFile file = fileUploadEvent.getFile();
+        System.out.println(file.getClass().getName());
+    }
 
-		return imageService;
-	}
+    public ImageService getImageService() {
 
-	public void setImageService(ImageService imageService) {
+        return imageService;
+    }
 
-		this.imageService = imageService;
-	}
+    public void setImageService(ImageService imageService) {
 
-	public File getImagem() {
+        this.imageService = imageService;
+    }
 
-		return imagem;
-	}
+    public Part getImagem() {
+        return imagem;
+    }
 
-	public void setImagem(File imagem) {
-
-		this.imagem = imagem;
-	}
-
+    public void setImagem(Part imagem) {
+        this.imagem = imagem;
+    }
 }
